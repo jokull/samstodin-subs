d7a7342fc7df1870600a07587d62076b3df83ec6
Jökull Sólberg <jokull@solberg.is>
2023-05-16 17:51:04 +0000

    tsc

--- app/kennitala.ts
+++ app/kennitala.ts
@@ -3,25 +3,20 @@ export function isValidKennitala(kennitala: string): boolean {
   if (kt.length !== 10) return false;
 
   const list = kt.split("").map(Number);
-  console.log({ list });
-
   if (list.find((num) => Number.isNaN(num))) {
     return false;
   }
 
   // Check if first two digits are between 1-72 and not between 31-41
   const dd = parseInt(list[0].toString() + list[1], 10);
-  console.log(1);
   if (!(dd > 0 && dd < 72) || (dd > 31 && dd < 41)) return false;
 
   // Check if third and fourth digits are between 1-12
   const mm = parseInt(list[2].toString() + list[3], 10);
-  console.log(2);
   if (!(mm > 0 && mm < 13)) return false;
 
   // Check if last digit is 0, 8 or 9
   const lastDigit = list[9];
-  console.log(3);
   if (!(lastDigit === 0 || lastDigit === 8 || lastDigit === 9)) return false;
 
   // Check control number
