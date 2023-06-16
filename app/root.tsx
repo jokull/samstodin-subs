0ae7863015095db0d980a251204daf7ab32f7775
Jökull Sólberg <jokull@solberg.is>
2023-06-16 21:27:20 +0000

    Open graph image

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta property="og:image" content="/opengraph.jpg"></meta>
         <Meta />
         <Links />
       </head>
