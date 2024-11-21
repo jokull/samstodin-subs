5fb0f0bc23161079a898170d81007bc288f2c38c
Jökull Sólberg <jokull@solberg.is>
2024-11-21 14:44:22 +0000

    wip

--- app/gleymt-lykilord/page.tsx
+++ app/gleymt-lykilord/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
+
 import { Form } from "./_components/form";
 
 export default function Page() {
   return (
     <div className="flex min-h-full flex-col justify-center">
       <div className="mx-auto w-full max-w-md px-4">
-        <Form />
+        <Suspense>
+          <Form />
+        </Suspense>
       </div>
     </div>
   );
